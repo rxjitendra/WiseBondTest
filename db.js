ba659180e01d92c0cc8fabdad748ba5b644f7ed5
rxjitendra <jitendra@eapfs.co.za>
2025-06-25 19:01:33 +0530

    add console

--- db.js
+++ db.js
@@ -6,7 +6,7 @@ async function getPostgresClient() {
     const pgHost = process.env.PGHOST || "wisebond-server.postgres.database.azure.com";
     const database = process.env.PGDATABASE || "postgres";
     const user = process.env.PGUSER || "wisebondtest";
-
+console.log(user);
     const tokenResponse = await credential.getToken("https://ossrdbms-aad.database.windows.net");
 
     const client = new Client({
