9ba8ad4989d23aaccc10b3a93897a24185e9f03a
Karan Tekwani <karan.tekwani@radixweb.com>
2025-06-26 13:52:39 +0530

    trim key vault values

--- db.js
+++ db.js
@@ -3,10 +3,10 @@ const { Client } = require("pg");
 
 async function getPostgresClient() {
     const credential = new DefaultAzureCredential();
-    const host = process.env["database_host"];
-    const database = process.env["database_name"];
-    const user = process.env["database_user"];
-    const port = process.env["database_port"];
+    const host = process.env["database_host"].trim();
+    const database = process.env["database_name"].trim();
+    const user = process.env["database_user"].trim();
+    const port = process.env["database_port"].trim();
 
     const tokenResponse = await credential.getToken("https://ossrdbms-aad.database.windows.net");
     const password = tokenResponse.token;
