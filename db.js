ceb9a38da0f74475835fd9de38ac63e4017d6f52
Karan Tekwani <karan.tekwani@radixweb.com>
2025-06-26 13:41:58 +0530

    console added to check key vault connected or not

--- db.js
+++ db.js
@@ -3,13 +3,19 @@ const { Client } = require("pg");
 
 async function getPostgresClient() {
     const credential = new DefaultAzureCredential();
-    const host = process.env["database-host"];
-    const database = process.env["database-name"];
-    const user = process.env["database-user"];
-    const port = process.env["database-port"];
+    const host = process.env["database_host"];
+    const database = process.env["database_name"];
+    const user = process.env["database_user"];
+    const port = process.env["database_port"];
 
     const tokenResponse = await credential.getToken("https://ossrdbms-aad.database.windows.net");
     const password = tokenResponse.token;
+
+    console.log(host)
+    console.log(database)
+    console.log(port)
+    console.log(user)
+    console.log(password)
 
     const client = new Client({
         host,
